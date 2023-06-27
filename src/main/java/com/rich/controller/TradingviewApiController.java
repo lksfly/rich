a752f1888e45efb3dfcf6b70189839af4ca22485
lksfly <bonobonoks@naver.com>
2023-06-27 16:44:47 +0900

    bybit 계정 잔고확인 코드 작성중

--- src/main/java/com/rich/controller/TradingviewApiController.java
+++ src/main/java/com/rich/controller/TradingviewApiController.java
@@ -1,5 +1,16 @@
 package com.rich.controller;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +20,122 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @RestController
 public class TradingviewApiController {
 	
 	private Gson gson = new Gson();
 	
+	
+	final private String apiDomainUrl = "https://api.bybit.com";
+	final private String getWB = "/v5/account/wallet-balance";
+	
+	
+	final static String API_KEY = "LGPINISDZRMULCFELE";
+    final static String API_SECRET = "LVRWFWUXKUKRGIXPZZCGOCSUFRIADYLRKXOE";
+    final static String TIMESTAMP = Long.toString(ZonedDateTime.now().toInstant().toEpochMilli());
+    final static String RECV_WINDOW = "5000";
+	
+	
 	@RequestMapping("/tradingview-alerts")
-	public void receiveAlert(@RequestBody String alertData) {
-		System.out.println("hi~~~~");
-		JsonObject jsonObject = gson.fromJson(alertData, JsonObject.class);
+	public void receiveAlert(@RequestBody String alertData) throws InvalidKeyException, NoSuchAlgorithmException {
+		System.out.println("hi~~~~!S");
+		
+		//JsonPrimitive jsonPrimitive = gson.fromJson(alertData, JsonPrimitive.class);
         // Alert 데이터 처리 코드 추가
 		System.out.println("hi~~~~");
         System.out.println(alertData);
+        
+        
+        Map<String, Object> map = new HashMap<>();
+        map.put("category","linear");
+        map.put("symbol", "BTCUSDT");
+        map.put("settleCoin", "USDT");
+
+        String signature = genGetSign(map);
+        StringBuilder sb = genQueryStr(map);        
+        
+        
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url("https://api.bybit.com/v5/account/wallet-balance")
+                .get()
+                .addHeader("X-BAPI-API-KEY", API_KEY)
+                .addHeader("X-BAPI-SIGN", signature)
+                .addHeader("X-BAPI-SIGN-TYPE", "2")
+                .addHeader("X-BAPI-TIMESTAMP", TIMESTAMP)
+                .addHeader("X-BAPI-RECV-WINDOW", RECV_WINDOW)
+                .build();
+        Call call = client.newCall(request);
+        try {
+            Response response = call.execute();
+            assert response.body() != null;
+            System.out.println(response.body().string());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        
+         
+        
+    }
+	
+	
+	  /**
+     * The way to generate the sign for GET requests
+     * @param params: Map input parameters
+     * @return signature used to be a parameter in the header
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    public String genGetSign(Map<String, Object> params) throws NoSuchAlgorithmException, InvalidKeyException {
+        StringBuilder sb = genQueryStr(params);
+        String queryStr = TIMESTAMP + API_KEY + RECV_WINDOW + sb;
+
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(API_SECRET.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+        return bytesToHex(sha256_HMAC.doFinal(queryStr.getBytes()));
+    }
+    
+    /**
+     * To convert bytes to hex
+     * @param hash
+     * @return hex string
+     */
+    public String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    /**
+     * To generate query string for GET requests
+     * @param map
+     * @return
+     */
+    public StringBuilder genQueryStr(Map<String, Object> map) {
+        Set<String> keySet = map.keySet();
+        Iterator<String> iter = keySet.iterator();
+        StringBuilder sb = new StringBuilder();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            sb.append(key)
+                    .append("=")
+                    .append(map.get(key))
+                    .append("&");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb;
     }
 	
 
