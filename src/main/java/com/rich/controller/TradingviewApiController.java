d361147a3b2e152d758bb68cbda34f137d01327f
lksfly <bonobonoks@naver.com>
2023-06-30 17:08:59 +0900

    코인 매수 기능 완성, todo: 하드코딩 변경하기

--- src/main/java/com/rich/controller/TradingviewApiController.java
+++ src/main/java/com/rich/controller/TradingviewApiController.java
@@ -15,73 +15,123 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.alibaba.fastjson.JSON;
+import org.json.*;  
 
 import okhttp3.Call;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 @RestController
 public class TradingviewApiController {
-	
-	private Gson gson = new Gson();
 	
 	
 	final private String apiDomainUrl = "https://api.bybit.com";
 	final private String getWB = "/v5/account/wallet-balance";
 	
 	
-	final static String API_KEY = "LGPINISDZRMULCFELE";
-    final static String API_SECRET = "LVRWFWUXKUKRGIXPZZCGOCSUFRIADYLRKXOE";
+	final static String API_KEY = "Hrv70SPyEbnYG1TDSl";
+    final static String API_SECRET = "r5J7hkl2AZfjmeqjAttu1Cze79u4wtaCCt2i";
     //final static String TIMESTAMP = Long.toString(ZonedDateTime.now().toInstant().toEpochMilli());
     final static String TIMESTAMP = Long.toString(new java.sql.Timestamp(System.currentTimeMillis()).getTime());
     final static String RECV_WINDOW = "50000";
 	
+    
+    public void placeOrder() throws InvalidKeyException, NoSuchAlgorithmException {
+    	System.out.println("===========placeOrder=====================");
+    	 Map<String, Object> map = new HashMap<>();
+         map.put("category", "linear"); //linear : USDT perpetual, and USDC contract, including USDC perp, USDC futures
+         map.put("symbol", "BTCUSDT");
+         map.put("side", "Buy");  //buy or sell
+         map.put("orderType", "Market");
+         map.put("qty", "1");
+         map.put("positionIdx", "1");
+         
+         String signature = genPostSign(map);
+         String jsonMap = JSON.toJSONString(map);
+         
+         OkHttpClient client = new OkHttpClient().newBuilder().build();
+         MediaType mediaType = MediaType.parse("application/json");
+         
+         
+         RequestBody requestBody = RequestBody.Companion.create(jsonMap,mediaType);
+         System.out.println("requestBody:"+requestBody.toString());
+         
+         Request request = new Request.Builder()
+                 .url("https://api-testnet.bybit.com/v5/order/create")
+                 .post(requestBody)
+                 .addHeader("X-BAPI-API-KEY", API_KEY)
+                 .addHeader("X-BAPI-SIGN", signature)
+                 .addHeader("X-BAPI-SIGN-TYPE", "2")
+                 .addHeader("X-BAPI-TIMESTAMP", TIMESTAMP)
+                 .addHeader("X-BAPI-RECV-WINDOW", RECV_WINDOW)
+                 .addHeader("Content-Type", "application/json")
+                 .build();
+         Call call = client.newCall(request);
+         try {
+             Response response = call.execute();
+             System.out.println("response:"+response);
+             assert response.body() != null;
+             System.out.println("~~~");
+             System.out.println("!!!!!!!!!"+response.body().string());
+         }catch (IOException e){
+             e.printStackTrace();
+         }
+         
+    }
+    
 	
 	@RequestMapping("/tradingview-alerts")
-	public void receiveAlert(@RequestBody String alertData) throws InvalidKeyException, NoSuchAlgorithmException {
+	public void receiveAlert( String alertData) throws InvalidKeyException, NoSuchAlgorithmException {
 		System.out.println("hi~~~~!S");
 		
 		//JsonPrimitive jsonPrimitive = gson.fromJson(alertData, JsonPrimitive.class);
         // Alert 데이터 처리 코드 추가
-		System.out.println("hi~~~~");
         System.out.println(alertData);
         
+        //Gson g = new Gson();
+        //Gson s = g.fromJson(alertData);  
+       
         //?accountType=CONTRACT&coin=USDT
-        Map<String, Object> map = new HashMap<>();
-        map.put("accountType", "CONTRACT");
-        map.put("coin", "USDT");
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("accountType", "CONTRACT");
+//        map.put("coin", "USDT");
 
-        String signature = genGetSign(map);
-        StringBuilder sb = genQueryStr(map);        
         
+        placeOrder();
         
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder()
-                .url("https://api.bybit.com/v5/account/wallet-balance?" + sb)
-                .get()
-                .addHeader("X-BAPI-API-KEY", API_KEY)
-                .addHeader("X-BAPI-SIGN", signature)
-                .addHeader("X-BAPI-SIGN-TYPE", "2")
-                .addHeader("X-BAPI-TIMESTAMP", TIMESTAMP)
-                .addHeader("X-BAPI-RECV-WINDOW", RECV_WINDOW)
-                .build();
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            System.out.println("response:"+response);
-            assert response.body() != null;
-            System.out.println("~~~");
-            System.out.println("!!!!!!!!!"+response.body().string());
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+//        String signature = genGetSign(map);
+//        StringBuilder sb = genQueryStr(map);        
+//        
+//        
+//        OkHttpClient client = new OkHttpClient().newBuilder().build();
+//        Request request = new Request.Builder()
+//                .url("https://api.bybit.com/v5/account/wallet-balance?" + sb)
+//                .get()
+//                .addHeader("X-BAPI-API-KEY", API_KEY)
+//                .addHeader("X-BAPI-SIGN", signature)
+//                .addHeader("X-BAPI-SIGN-TYPE", "2")
+//                .addHeader("X-BAPI-TIMESTAMP", TIMESTAMP)
+//                .addHeader("X-BAPI-RECV-WINDOW", RECV_WINDOW)
+//                .build();
+//        Call call = client.newCall(request);
+//        try {
+//            Response response = call.execute();
+//            System.out.println("response:"+response);
+//            assert response.body() != null;
+//            System.out.println("~~~");
+//            System.out.println("!!!!!!!!!"+response.body().string());
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
         
          
         
@@ -103,6 +153,26 @@ public class TradingviewApiController {
         SecretKeySpec secret_key = new SecretKeySpec(API_SECRET.getBytes(), "HmacSHA256");
         sha256_HMAC.init(secret_key);
         return bytesToHex(sha256_HMAC.doFinal(queryStr.getBytes()));
+    }
+    
+    
+    /**
+     * The way to generate the sign for POST requests
+     * @param params: Map input parameters
+     * @return signature used to be a parameter in the header
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     */
+    public String genPostSign(Map<String, Object> params) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(API_SECRET.getBytes(), "HmacSHA256");
+        sha256_HMAC.init(secret_key);
+
+        String paramJson = JSON.toJSONString(params);
+        System.out.println("paramJson: "+ paramJson);
+        //String paramJson = params.toString();        
+        String sb = TIMESTAMP + API_KEY + RECV_WINDOW + paramJson;
+        return bytesToHex(sha256_HMAC.doFinal(sb.getBytes()));
     }
     
     /**
@@ -137,7 +207,6 @@ public class TradingviewApiController {
                     .append("&");
         }
         sb.deleteCharAt(sb.length() - 1);
-        System.out.println("sb==============="+sb);
         return sb;
     }
 	
